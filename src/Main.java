e7e0350a78eb5f1a3c7bc8a16c2466905c47dc38
isaac <isaac.j.mayo@gmail.com>
2025-03-30 09:19:10 -0700

    Sound code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void playSound(File Audio) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Audio.getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            Thread.sleep(1000);
+        } catch (Exception ex) {
+            System.out.println("Failed to run audio");
+            ex.printStackTrace();
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        File Test = new File("C:\\Users\\isaac\\Downloads\\Symbal.wav");
+playSound(Test);
     }
 }
