ff4951a9a95428cb5699d84c2a3f5ada4ed76fb8
isaac <isaac.j.mayo@gmail.com>
2025-03-30 09:38:28 -0700

    Sound code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void playSound(File Audio) {
@@ -15,19 +16,26 @@ public class Main {
             ex.printStackTrace();
         }
     }
-
-
-
-
-
-
-
-
-
-
-
+    public static void prepareMicrophone() {
+        ArrayList<Byte> Test = new ArrayList<>();
+        try {
+            AudioFormat format = new AudioFormat(48100, 16, 1, true, true);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
+            microphone.open(format);
+            microphone.start();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = microphone.read(buffer, 0, buffer.length)) > 0) {
+                Test.add(buffer[buffer.length - 1]);
+            }
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
         File Test = new File("C:\\Users\\isaac\\Downloads\\Symbal.wav");
-playSound(Test);
+        prepareMicrophone();
+
     }
 }
